fca804fbb589bd9f777934c2b97381b876e84fb2
Oscar Menjivar <omenjivar@sapient.com>
2017-11-01 15:31:34 -0400

    First Commit, Empty Project

---
+++ app/src/main/java/safereef/scaro/com/myapplication/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package safereef.scaro.com.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
